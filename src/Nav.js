b6d653741b40d7bde3a12854af352337acd67595
asher scott <asher.scott6@gmail.com>
2022-04-19 09:06:57 -0500

    nav

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="navWrapper">
+      <nav>
+        <a>Game List</a>
+        <a>Settings</a>
+        <a>Game Form</a>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
