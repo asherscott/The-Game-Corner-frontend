215971b2f9424f31153a81fd5165dbfe301f738b
asher scott <asher.scott6@gmail.com>
2022-04-19 12:19:34 -0500

    component Setting to Settings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import Home from "./Home";
 import Game from "./Game";
-import Setting from "./Setting"
-import Search from "./Search"
 
 function App() {
   return (
     <div>
       <Home />
       <Game />
-      <Setting ></Setting>
     </div>
   );
 }
