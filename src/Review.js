08344ad5dd990a89f40bdf9dc243f8ae52573085
asher scott <asher.scott6@gmail.com>
2022-04-20 18:56:00 -0500

    review like / dislike working

--- src/Review.js
+++ src/Review.js
@@ -1,7 +1,25 @@
 import ReactStars from "react-rating-stars-component";
 import "./Review.css";
+import { useState } from "react";
 
 function Review({ review }) {
+  const [likes, setLikes] = useState(review.likes);
+
+  function handleLike(e) {
+    const newLike = e.target.name === "like" ? likes + 1 : likes - 1;
+
+    const { ["username"]: username, ...reviewNoUsername } = review;
+
+    const config = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...reviewNoUsername, ...{ likes: newLike } }),
+    };
+
+    fetch(`http://localhost:9292/reviews/${review.id}`, config)
+      .then((r) => r.json())
+      .then((rev) => setLikes(rev.likes));
+  }
   return (
     <article>
       <header className="reviewHead">
@@ -13,6 +31,15 @@ function Review({ review }) {
       </header>
 
       <p className="comment">{review.comment}</p>
+      <div className="comment">
+        <span>{likes}</span>
+        <button onClick={handleLike} name="like" className="revBtn like">
+          /\
+        </button>
+        <button onClick={handleLike} name="dislike" className="revBtn dislike">
+          \/
+        </button>
+      </div>
       <br />
     </article>
   );
